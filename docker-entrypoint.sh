a6c03b04408dc39e30e467145faf48f7ff606699
Kirill A. Korinsky <kirill@korins.ky>
2017-02-17 11:55:04 +0100

    Add docker-entrypoint.d

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -52,4 +52,12 @@ done
 
 /etc/init.d/cron start
 
+for f in /docker-entrypoint.d/*; do
+  case "$f" in
+				*.sh)     echo "$0: running $f"; "$f" ;;
+				*)        echo "$0: ignoring $f" ;;
+	esac
+  echo
+done
+
 nginx -g 'daemon off;'
